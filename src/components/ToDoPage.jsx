703ad1d888182a914a280cc7001a5f41aeb325b5
iznoelc <izzybellanoel@gmail.com>
2026-03-13 14:13:50 -0400

    fix cally

--- src/components/ToDoPage.jsx
+++ src/components/ToDoPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
 export default function ToDoPage() {
   // List of tasks: each task has { task: string, date: string }
   const [tasksList, setTasksList] = useState([]);
@@ -82,6 +81,7 @@ export default function ToDoPage() {
       </button>
 
       <calendar-date className="cally bg-base-100 border border-base-300 shadow-lg rounded-box">
+        <h1>test</h1>
         <svg aria-label="Previous" className="fill-current size-4" slot="previous" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path></svg>
         <svg aria-label="Next" className="fill-current size-4" slot="next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path></svg>
         <calendar-month></calendar-month>
